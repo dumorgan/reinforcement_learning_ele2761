3d840e8c0c492bffe9677ce345c467f6fa380c02
Eduardo Morgan <eduardo.morgan@greenant.com.br>
2019-03-27 17:19:22 -0300

    implements policy evaluation

--- ex1/ex_1.py
+++ ex1/ex_1.py
@@ -1,7 +1,21 @@
 from .gridworld import gridworld
+import numpy as np
 
-def iterative_policy_estimation(theta, S):
+
+def iterative_policy_evaluation(pi, S, theta=1e-5, gamma=0.9):
+    """
+    
+    """
+    S = S if S is not None else np.range(start=0, stop=5*5, step=1)
     delta = 0
 
-    for s in S:
-        v = 
+    V = np.random.random_sample(S)
+    V[-1] = 0
+
+    while delta >= theta:
+        for s in S:
+            v = V[s]
+            reward, next_state = gridworld(s, pi(s))
+            V[s] = reward + gamma * V[next_state]
+            delta = max(delta, abs(v - V[s]))
+
