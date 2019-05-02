01ecb9f151534da043c8094d4276eca096e031ae
Eduardo Morgan <eduardo.morgan@greenant.com.br>
2019-05-02 18:12:58 -0300

    implements LSPI, still needs testing but looking good

---
+++ ex3/ipdynamics.m
@@ -0,0 +1,25 @@
+function xd = ipdynamics(x, u)
+%IPDYNAMICS Inverted pendulum dynamics
+%   XD = IPDYNAMICS(X, U) returns the state vector derivative given a
+%   current state X = [THETA, DTHETA] and action U = [MOTOR_VOLTAGE]
+%
+%   AUTHOR:
+%      Wouter Caarls <wouter@caarls.org>
+
+  J_ = 0.000191;
+  m_ = 0.055;
+  g_ = 9.81;
+  l_ = 0.042;
+  b_ = 0.000003;
+  K_ = 0.0536;
+  R_ = 9.5;
+
+  a = x(1);
+  ad = x(2);
+  add = (1/J_)*(m_*g_*l_*sin(a)-b_*ad-(K_*K_/R_)*ad+(K_/R_)*u);
+
+  xd = x;
+  xd(1) = ad;
+  xd(2) = add;
+  
+end
